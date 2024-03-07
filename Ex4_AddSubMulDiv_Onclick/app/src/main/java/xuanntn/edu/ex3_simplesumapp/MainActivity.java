e2f6234d0ac29c6b064e78203d2c6cafbc027157
nhatxun2811 <xxuan0621@gmail.com>
2024-03-07 11:53:10 +0700

    Update Exercise

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/xuanntn/edu/ex3_simplesumapp/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/xuanntn/edu/ex3_simplesumapp/MainActivity.java
@@ -2,6 +2,7 @@ package xuanntn.edu.ex3_simplesumapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -12,11 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtso1;
+    EditText edtso2;
+    EditText edtKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
+
+    void TimDieuKhien(){
+        edtso1 = findViewById(R.id.edt1);
+        edtso2 = findViewById(R.id.edt2);
+        edtKQ = findViewById(R.id.edtKQ);
+        nutCong = findViewById(R.id.btnC);
+        nutTru = findViewById(R.id.btnT);
+        nutNhan = findViewById(R.id.btnN);
+        nutChia = findViewById(R.id.btnCh);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,20 +40,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    public void XuLyCong(View view){
-//        EditText edtsoA = findViewById(R.id.txtA);
-//        EditText edtsoB = findViewById(R.id.txtB);
-//        EditText edtKQ = findViewById(R.id.txtKQ);
-//
-//        String strA = edtsoA.getText().toString();
-//        String strB = edtsoB.getText().toString();
-//
-//        double soA = Double.parseDouble(strA);
-//        double soB = Double.parseDouble(strB);
-//
-//        double tong = soA + soB;
-//        String strTong = String.valueOf(tong);
-//
-//        edtKQ.setText(strTong);
-//    }
+    public void XuLyCong(View view){
+
+        String strA = edtso1.getText().toString();
+        String strB = edtso2.getText().toString();
+
+        double so1 = Double.parseDouble(strA);
+        double so2 = Double.parseDouble(strB);
+
+        double tong = so1 + so2;
+        String strTong = String.valueOf(tong);
+
+        edtKQ.setText(strTong);
+    }
+    public void XuLyTru(View view){
+
+        String strA = edtso1.getText().toString();
+        String strB = edtso2.getText().toString();
+
+        double so1 = Double.parseDouble(strA);
+        double so2 = Double.parseDouble(strB);
+
+        double hieu = so1 - so2;
+        String strHieu = String.valueOf(hieu);
+
+        edtKQ.setText(strHieu);
+    }
+    public void XuLyNhan(View view){
+
+        String strA = edtso1.getText().toString();
+        String strB = edtso2.getText().toString();
+
+        double so1 = Double.parseDouble(strA);
+        double so2 = Double.parseDouble(strB);
+
+        double tich = so1 * so2;
+        String strTich = String.valueOf(tich);
+
+        edtKQ.setText(strTich);
+    }
+    public void XuLyChia(View view){
+
+        String strA = edtso1.getText().toString();
+        String strB = edtso2.getText().toString();
+
+        double so1 = Double.parseDouble(strA);
+        double so2 = Double.parseDouble(strB);
+
+        double thuong = so1 / so2;
+        String strThuong = String.valueOf(thuong);
+
+        edtKQ.setText(strThuong);
+    }
 }
