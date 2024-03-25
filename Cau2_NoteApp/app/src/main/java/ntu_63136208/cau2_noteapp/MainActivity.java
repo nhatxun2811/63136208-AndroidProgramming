b3f0d97fe88f55316c9b2cbf6e5553e5c27e21b2
nhatxun2811 <xxuan0621@gmail.com>
2024-03-25 22:08:41 +0700

    Thêm dữ liệu và hoạt động của nút thêm note

--- Cau2_NoteApp/app/src/main/java/ntu_63136208/cau2_noteapp/MainActivity.java
+++ Cau2_NoteApp/app/src/main/java/ntu_63136208/cau2_noteapp/MainActivity.java
@@ -1,24 +1,46 @@
 package ntu_63136208.cau2_noteapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import java.util.ArrayList;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText noteInput;
+    Button addButton;
+    TextView noteDisplay;
+    ArrayList<String> notes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        noteInput = findViewById(R.id.note_input);
+        addButton = findViewById(R.id.btnAdd);
+        noteDisplay = findViewById(R.id.note_display);
+        notes = new ArrayList<>();
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String note = noteInput.getText().toString();
+                notes.add(note);
+                displayNotes();
+                noteInput.setText("");
+            }
         });
+    }
+    private void displayNotes() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String note : notes) {
+            stringBuilder.append(note).append("\n");
+        }
+        noteDisplay.setText(stringBuilder.toString());
     }
 }
