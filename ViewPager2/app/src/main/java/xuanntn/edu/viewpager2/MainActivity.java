5e4760ee5ba79de152a05734b9051ca6d3a90fc2
nhatxun2811 <xxuan0621@gmail.com>
2024-04-16 09:19:39 +0700

    Update ViewPager2

---
+++ ViewPager2/app/src/main/java/xuanntn/edu/viewpager2/MainActivity.java
@@ -0,0 +1,52 @@
+package xuanntn.edu.viewpager2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> viewPagerData;
+
+    ViewPager2 viewPager2Land;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPagerData = getDataForViewPager();
+        viewPager2Land = findViewById(R.id.vp2Land);
+        landScapeAdapter = new LandScapeAdapter(this, viewPagerData);
+        viewPager2Land.setAdapter(landScapeAdapter);
+        viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    ArrayList<LandScape> getDataForViewPager() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<>();
+        dsDuLieu.add(new LandScape("halongbay", "Vịnh Hạ Long"));
+        dsDuLieu.add(new LandScape("banahills", "Bà Nà Hills"));
+        dsDuLieu.add(new LandScape("vinwonder", "VinWonder Phú Quốc"));
+        dsDuLieu.add(new LandScape("thaphanoi", "Cột cờ Hà Nội"));
+        dsDuLieu.add(new LandScape("effel", "Tháp Effel"));
+        return dsDuLieu;
+    }
+}
